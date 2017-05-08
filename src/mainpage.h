734610d6212fc2e56147e6485e8ffd8ae25aca19
H00N24 <ondrej.kurak.h@gmail.com>
2017-05-08 13:29:58 +0200

    mainpage

---
+++ src/mainpage.h
@@ -0,0 +1,14 @@
+/*! \mainpage C++ Pasians - Solitaire
+ * Pasians written in C++ using Qt framework with both GUI and CLI
+ *
+ * \section authors_sec Authors
+ * Eduard Čuba <xcubae00@stud.fit.vutbr.cz>
+ *
+ * Ondrej Kurák <xkurak00@stud.fit.vutbr.cz>
+ *
+ * \section usage_sec Game files
+ * ./hra2017 -> game with GUI
+ *
+ * ./hra2017-cli -> game with CLI
+
+ */
